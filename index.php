7415de05008f211dde0900393ef6b5fbd7f747dd
fatiha Elk <fatiha.elkarrouti@yahoo.fr>
2020-11-19 15:51:10 +0100

    modif_index

--- index.php
+++ index.php
@@ -4,29 +4,11 @@ require_once 'inc/nav.inc.php';
 ?>
         
 
-        <h1 class="bg-light display-4 text-center"> POKEMON LIFE </h1><br>
+        <h1> ANNULATION TOTAL </h1>
 
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="photo/pikachu.jfif" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="photo/sacha.jfif" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="photo/duo.jfif" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a><br>
-        </div>
+        <p class="mx-auto text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam facilis ipsa delectus ducimus quam quibusdam unde ad ipsum voluptas minima voluptate maxime animi dolorum magni, perspiciatis sed tenetur? Ut aperiam culpa, quidem qui libero ea sed explicabo aliquid repellat perspiciatis odio illo soluta nostrum ducimus dicta numquam hic quia doloremque deleniti officia, totam eligendi eos alias asperiores. Corrupti molestiae voluptatibus veniam. Adipisci et fuga placeat, ducimus perferendis corporis laboriosam, eligendi quos corrupti sed, molestias at architecto suscipit numquam quod? Error repudiandae ullam amet omnis quam vel similique voluptatem quo impedit totam, ipsum enim! Sequi, nulla praesentium. Blanditiis, commodi ex?</p>
+
+        <img src="photo/sacha.jfif" alt="">
         <?php
 require_once 'inc/footer.inc.php';
     
